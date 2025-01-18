d62cd640b5a4868911b12febf0ed875f8ccda605
Ian Fung <ianfung9264@hotmail.com>
2025-01-18 21:20:05 +0800

    abc

--- src/pages/Approve/urgent/detailModal.tsx
+++ src/pages/Approve/urgent/detailModal.tsx
@@ -1,154 +1,204 @@
-import { _approveCancelEvent, _getEventById, _getEventDetailsByEventId } from "@/services/event/info";
+import {
+  _approveCancelEvent,
+  _getEventById,
+  _getEventDetailsByEventId,
+} from "@/services/event/info";
 import { Button, message, Modal, Input } from "antd";
 import { useEffect, useState } from "react";
 
 interface DetailModalProps {
-	initData: {
-		landownerId: string;
-		wallet: number;
-		eventId: string;
-		eventName: string;
-	};
-	mainTableReload?: () => Promise<void>;
+  initData: {
+    landownerId: string;
+    wallet: number;
+    eventId: string;
+    eventName: string;
+  };
+  mainTableReload?: () => Promise<void>;
 }
-const DetailModal: React.FC<DetailModalProps> = ({ initData, mainTableReload }) => {
-	const [isModalOpen, setIsModalOpen] = useState(false);
+const DetailModal: React.FC<DetailModalProps> = ({
+  initData,
+  mainTableReload,
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-	const [eventData, setEventData] = useState<API_EVENT.Event>();
-	const [loading, setLoading] = useState(false);
-	const [totalAmount, setTotalAmount] = useState(0);
-	const [paidParticipant, setPaidParticipant] = useState(0);
-	const [applicationIdList, setApplicationIdList] = useState([]);
-	const [isRefundModalOpen, setIsRefundModalOpen] = useState(false);
-	const [refundAmount, setRefundAmount] = useState<string>("");
+  const [eventData, setEventData] = useState<API_EVENT.Event>();
+  const [loading, setLoading] = useState(false);
+  const [totalAmount, setTotalAmount] = useState(0);
+  const [paidParticipant, setPaidParticipant] = useState(0);
+  const [applicationIdList, setApplicationIdList] = useState([]);
+  const [isRefundModalOpen, setIsRefundModalOpen] = useState(false);
+  const [refundAmount, setRefundAmount] = useState<string>("");
 
-	//fetch event data when modal open
-	useEffect(() => {
-		if (isModalOpen) {
-			fetchEventDataById();
-		}
-	}, [isModalOpen]);
+  //fetch event data when modal open
+  useEffect(() => {
+    if (isModalOpen) {
+      fetchEventDataById();
+    }
+  }, [isModalOpen]);
 
-	const fetchEventDataById = async () => {
-		setLoading(true);
-		try {
-			const { data } = await _getEventById(initData.eventId);
-			setEventData(data);
-			console.log("initData", data);
-			const eventDetail = await _getEventDetailsByEventId(initData.landownerId, initData.eventId);
-			console.log("eventDetail", eventDetail);
-			console.log("just testing");
-			// const totalAmount = eventDetail.application_list.reduce((sum: number, app: any) => {
-			const totalAmount = eventDetail[0].application_list.reduce((sum: number, app: any) => {
-				if (app.status === "Paid") {
-					return sum + (app.ticket_type?.amount || 0);
-				}
-				return sum;
-			}, 0);
-			setTotalAmount(totalAmount);
-			console.log("totalAmount", totalAmount);
+  const fetchEventDataById = async () => {
+    setLoading(true);
+    try {
+      const { data } = await _getEventById(initData.eventId);
+      setEventData(data);
+      console.log("initData", data);
+      const eventDetail = await _getEventDetailsByEventId(
+        initData.landownerId,
+        initData.eventId
+      );
+      console.log("eventDetail", eventDetail);
 
-			const paidParticipant = eventDetail[0].application_list.filter((app: any) => app.status === "Paid").length;
-			console.log("paidParticipant", paidParticipant);
-			setPaidParticipant(paidParticipant);
+      const totalAmount = eventDetail[0].application_list.reduce(
+        (sum: number, app: any) => {
+          if (app.status === "Paid") {
+            const ticketAmount = app.ticket_type?.amount || 0;
 
-			const application_id_list = eventDetail[0].application_list
-				.filter((app: any) => app.status === "Paid")
-				.map((app: any, index: any) => {
-					return {
-						id: app.application_id,
-						amount: app.ticket_type?.amount,
-					};
-				});
-			setApplicationIdList(application_id_list);
-		} catch (error) {
-			message.error("Failed to fetch event data");
-		} finally {
-			setLoading(false);
-		}
-	};
+            const addOnTotal =
+              app.add_on?.reduce((addOnSum: number, addon: any) => {
+                return addOnSum + addon.quantity * addon.amount;
+              }, 0) || 0;
 
-	const handleRefundModalOk = async () => {
-		if (!refundAmount || isNaN(Number(refundAmount))) {
-			message.error("Please enter a valid refund amount");
-			return;
-		}
-		setIsRefundModalOpen(false);
-		const newApplicationIdListForRefundPayload = applicationIdList?.map((app: any) => ({
-			id: app.id,
-			amount: Number(refundAmount),
-		}));
-		const payload = {
-			application_id_list: newApplicationIdListForRefundPayload,
-		};
-		console.log("payload", payload);
-		const result = await _approveCancelEvent(payload, initData.landownerId);
-		if (result.code === 400) {
-			message.error(result.message);
-		} else {
-			message.success("Event cancellation approved successfully");
-			setIsModalOpen(false);
-			mainTableReload?.();
-		}
-	};
+            const appTotal = ticketAmount + addOnTotal;
+            console.log("Application Total:", appTotal);
 
-	return (
-		<>
-			<Button type="link" onClick={() => setIsModalOpen(true)}>
-				View Details
-			</Button>
-			<Modal
-				title="Landowner Details"
-				open={isModalOpen}
-				onCancel={() => setIsModalOpen(false)}
-				footer={[
-					<Button key="reject" type="default" onClick={() => setIsModalOpen(false)}>
-						Reject Cancellation Request
-					</Button>,
-					<Button key="approve" type="primary" onClick={() => setIsRefundModalOpen(true)}>
-						Approve Event Cancellation
-					</Button>,
-				]}
-			>
-				<p>
-					<strong>Landowner ID:</strong> {initData.landownerId}
-				</p>
-				<p>
-					<strong>This Landowner's Wallet Balance:</strong> ${Number(initData.wallet).toFixed(2)}
-				</p>
-				<p>
-					<strong>Event ID:</strong> {initData.eventId}
-				</p>
-				<p>
-					<strong>Event Name:</strong> {initData.eventName}
-				</p>
-				<p>
-					<strong>Participants who already paid application fee:</strong> {paidParticipant}
-				</p>
-				<p>
-					<strong>Total Amount Received From Participants:</strong> ${totalAmount?.toFixed(2)}
-				</p>
-			</Modal>
-			<Modal
-				title="Enter Refund Amount"
-				open={isRefundModalOpen}
-				onCancel={() => setIsRefundModalOpen(false)}
-				onOk={handleRefundModalOk}
-			>
-				<div style={{ padding: "20px 0" }}>
-					<Input
-						prefix="$"
-						placeholder="Enter refund amount"
-						value={refundAmount}
-						onChange={(e) => setRefundAmount(e.target.value)}
-						type="number"
-						min="0"
-						step="0.01"
-					/>
-				</div>
-			</Modal>
-		</>
-	);
+            return sum + ticketAmount + addOnTotal;
+          }
+          return sum;
+        },
+        0
+      );
+      setTotalAmount(totalAmount);
+      console.log("totalAmount", totalAmount);
+
+      const paidParticipant = eventDetail[0].application_list.filter(
+        (app: any) => app.status === "Paid"
+      ).length;
+      console.log("paidParticipant", paidParticipant);
+      setPaidParticipant(paidParticipant);
+
+      const application_id_list = eventDetail[0].application_list
+        .filter((app: any) => app.status === "Paid")
+        .map((app: any, index: any) => {
+          return {
+            id: app.application_id,
+            amount: app.ticket_type?.amount,
+          };
+        });
+      setApplicationIdList(application_id_list);
+    } catch (error) {
+      message.error("Failed to fetch event data");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleRefundModalOk = async () => {
+    if (!refundAmount || isNaN(Number(refundAmount))) {
+      message.error("Please enter a valid refund amount");
+      return;
+    }
+    setIsRefundModalOpen(false);
+    const newApplicationIdListForRefundPayload = applicationIdList?.map(
+      (app: any) => ({
+        id: app.id,
+        amount: Number(refundAmount),
+      })
+    );
+    const payload = {
+      application_id_list: newApplicationIdListForRefundPayload,
+    };
+    console.log("payload", payload);
+    const result = await _approveCancelEvent(payload, initData.landownerId);
+    if (result.code === 400) {
+      message.error(result.message);
+    } else {
+      message.success("Event cancellation approved successfully");
+      setIsModalOpen(false);
+      mainTableReload?.();
+    }
+  };
+
+  return (
+    <>
+      <Button type="link" onClick={() => setIsModalOpen(true)}>
+        View Details
+      </Button>
+      <Modal
+        title="Landowner Details"
+        open={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+        footer={[
+          <Button
+            key="reject"
+            type="default"
+            onClick={() => setIsModalOpen(false)}
+          >
+            Reject Cancellation Request
+          </Button>,
+          <Button
+            key="approve"
+            type="primary"
+            onClick={() => setIsRefundModalOpen(true)}
+          >
+            Approve Event Cancellation
+          </Button>,
+        ]}
+      >
+        <p>
+          <strong>Landowner ID:</strong> {initData.landownerId}
+        </p>
+        <p>
+          <strong>This Landowner's Wallet Balance:</strong> $
+          {Number(initData.wallet).toFixed(2)}
+        </p>
+        <p>
+          <strong>Event ID:</strong> {initData.eventId}
+        </p>
+        <p>
+          <strong>Event Name:</strong> {initData.eventName}
+        </p>
+        <p>
+          <strong>Participants who already paid application fee:</strong>{" "}
+          {paidParticipant}
+        </p>
+        <p>
+          <strong>Total Amount Received From Participants:</strong> $
+          {totalAmount?.toFixed(2)}
+        </p>
+        <p>
+          <strong>Total Amount per Participant:</strong> $
+          {(totalAmount / paidParticipant).toFixed(2)}
+        </p>
+      </Modal>
+      <Modal
+        title="Enter Refund Amount"
+        open={isRefundModalOpen}
+        onCancel={() => setIsRefundModalOpen(false)}
+        onOk={handleRefundModalOk}
+      >
+        <div style={{ padding: "20px 0" }}>
+          <p>Enter the amount you want to refund PER PARTICIPANT</p>
+          <p>
+            if you want to refund 100% for each participant, enter $
+            {totalAmount / paidParticipant}
+          </p>
+          <p>
+            if you enter a number larger than {totalAmount / paidParticipant},
+            it won't work.
+          </p>
+          <Input
+            prefix="$"
+            placeholder="Enter refund amount"
+            value={refundAmount}
+            onChange={(e) => setRefundAmount(e.target.value)}
+            type="number"
+            min="0"
+            step="0.01"
+          />
+        </div>
+      </Modal>
+    </>
+  );
 };
 
 export default DetailModal;
