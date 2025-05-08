e8451872e7dc0fd34e91c6ff8893516f3c66dd58
Ian Fung <ianfung9264@hotmail.com>
2025-05-08 18:11:12 +0800

    a

--- src/pages/Transaction/refund/columns.tsx
+++ src/pages/Transaction/refund/columns.tsx
@@ -5,70 +5,86 @@ import { LandownerAdvancedStatus, OtpStatusType } from "@/services/commonType";
 import { InfoCircleOutlined } from "@ant-design/icons";
 
 export function RefundTableColumns({
-  mainTableReload,
+	mainTableReload,
 }: {
-  mainTableReload: (() => Promise<void>) | undefined;
+	mainTableReload: (() => Promise<void>) | undefined;
 }): ProColumns<any>[] {
-  // console.log("mainTableReload", mainTableReload);
-  // const [tableReload, setTableReload] = useState(() => mainTableReload);
+	// console.log("mainTableReload", mainTableReload);
+	// const [tableReload, setTableReload] = useState(() => mainTableReload);
 
-  // useEffect(() => {
-  //   setTableReload(() => mainTableReload);
-  // }, [mainTableReload]);
-  return [
-    {
-      title: "Event id",
-      dataIndex: "application.event_id",
-      key: "landowner",
-      render: (_, record) => {
-        return record.application ? record.application?.event_id : null;
-      },
-      align: "center",
-    },
-    {
-      title: "Application id",
-      dataIndex: "application_id",
-      key: "application_id",
-      align: "center",
-    },
-    {
-      title: "Amount",
-      dataIndex: "refunded_amount",
-      key: "amount",
-      align: "center",
-    },
-
-    {
-      title: "Currency",
-      dataIndex: "refunded_currency",
-      key: "currency",
-      align: "center",
-    },
-    {
-      title: "Progress",
-      dataIndex: "is_confirm_refunded",
-      key: "is_confirm_refunded",
-      align: "center",
-      valueType: "select",
-      valueEnum: {
-        false: "Pending",
-        true: "Confirmed",
-      },
-    },
-    {
-      title: "Refunded time",
-      dataIndex: "refunded_datetime",
-      key: "refunded_datetime",
-      align: "center",
-      valueType: "dateTime",
-    },
-
-    {
-      title: "_id",
-      dataIndex: "event_idCount",
-      key: "_id",
-      align: "center",
-      hidden: true,
-    },
-  ];
+	// useEffect(() => {
+	//   setTableReload(() => mainTableReload);
+	// }, [mainTableReload]);
+	return [
+		{
+			title: "Event Name",
+			dataIndex: ["application", "event_name"],
+			key: "event_name",
+			align: "center",
+			render: (_, record) => {
+				return record.application ? record.application?.event_name : null;
+			},
+		},
+		{
+			title: "Refund Policy",
+			dataIndex: ["application", "refund_policy"],
+			key: "refund_policy",
+			align: "center",
+			render: (_, record) => {
+				if (
+					!record.application ||
+					!record.application.refund_policy ||
+					!Array.isArray(record.application.refund_policy)
+				) {
+					return null;
+				}
+				return record.application.refund_policy
+					.map((policy: { days: string; percent: string }) => `${policy.days} days: ${policy.percent}%`)
+					.join(", ");
+			},
+		},
+		{
+			title: "Vendor Business Name",
+			dataIndex: ["application", "tenant_business_name"],
+			key: "vendor_business_name",
+			align: "center",
+			render: (_, record) => {
+				return record.application ? record.application?.tenant_business_name : null;
+			},
+		},
+		{
+			title: "Ticket Type",
+			key: "ticket_type",
+			align: "center",
+			render: (_, record) => {
+				return record.application && record.application.ticket_type
+					? record.application.ticket_type.ticket_type
+					: null;
+			},
+		},
+		{
+			title: "Amount",
+			dataIndex: "refunded_amount",
+			key: "amount",
+			align: "center",
+		},
+		{
+			title: "Progress",
+			dataIndex: "is_confirm_refunded",
+			key: "is_confirm_refunded",
+			align: "center",
+			valueType: "select",
+			valueEnum: {
+				false: "Pending",
+				true: "Confirmed",
+			},
+		},
+		{
+			title: "Refunded time",
+			dataIndex: "refunded_datetime",
+			key: "refunded_datetime",
+			align: "center",
+			valueType: "dateTime",
+		},
+	];
 }
